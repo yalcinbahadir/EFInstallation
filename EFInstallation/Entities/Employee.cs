b5c04de9778f9569465a2d66dd0d690a456b53b0
yalcinPXL <yalcinPXL@gmail.com>
2021-06-21 22:33:40 +0200

    Add Gender property

--- EFInstallation/Entities/Employee.cs
+++ EFInstallation/Entities/Employee.cs
@@ -11,7 +11,7 @@ namespace EFInstallation.Entities
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-       // public string Gender { get; set; }
+        public string Gender { get; set; }
         public DateTime? DateOfBirth { get; set; }
         public string Email { get; set; }
         public int DepartmentId { get; set; }
